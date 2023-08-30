4b3dde2dc136b0d10a2a9968f594f74c5f5fb87b
Zacharie Morrow <zacharie.morrow@gmail.com>
2023-08-30 19:29:29 -0400

    lines 13 and 18 changed for style

--- compare.c
+++ compare.c
@@ -10,12 +10,10 @@ int main(void)
    {
         printf("x is less than y\n");
    }
-
    else if (x > y)
    {
         printf("x is not less than y\n");
    }
-
    else
    {
         printf("x is equal to y\n");
