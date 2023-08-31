2b59fb57ea60cdd012ed858a824e6a3d6dace4c1
Zacharie Morrow <zacharie.morrow@gmail.com>
2023-08-30 23:07:17 -0400

    Grid Printer
    Prints grid with size equal to user input. Uses two functions other than main.

---
+++ mario.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int get_size(void);
+void print_grid(int size);
+
+int main(void)
+{
+    // get size of grid
+    int n = get_size();
+
+    // prints grid of bricks
+    print_grid(n);
+
+}
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    return n;
+}
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
